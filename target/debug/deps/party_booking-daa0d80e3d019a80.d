0c1c5b6c4b88ee86bd411ca4f293db5ff94cfc0a
pmatysiewicz <pmatysiewicz1@gmail.com>
2026-06-06 16:16:46 +0200

    new cargo project

---
+++ target/debug/deps/party_booking-daa0d80e3d019a80.d
@@ -0,0 +1,9 @@
+/home/pat/Dev/PartyBooking/party-booking/target/debug/deps/party_booking-daa0d80e3d019a80.d: src/main.rs Cargo.toml
+
+/home/pat/Dev/PartyBooking/party-booking/target/debug/deps/libparty_booking-daa0d80e3d019a80.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
